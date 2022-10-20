52fd030f0c25e5b7b2f2b85224be5d17070c0c94
AlessP93 <alespiscino@libero.it>
2022-10-20 17:19:31 +0200

    creata crud

--- Controllers/SmartBoxController.cs
+++ Controllers/SmartBoxController.cs
@@ -1,83 +1,113 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient.Server;
+using webapp_travel_agency.Models;
 
 namespace webapp_travel_agency.Controllers
 {
     public class SmartBoxController : Controller
     {
-        // GET: SmartBoxController
+        // db globale solo nel genitore 
+        SmartBoxContext sbc = new SmartBoxContext();
+
+        // GET: SmartBoxController -- Mostrai i dati tipo vetrina
         public ActionResult Index()
         {
-            return View();
+            
+            List<SmartBox> smartBox = sbc.smartBoxes.ToList();
+            return View("Index", sbc);
+           
         }
 
-        // GET: SmartBoxController/Details/5
+        // GET: SmartBoxController/Details/5 -- Mostra solo i dati singoli "dettaglio"
         public ActionResult Details(int id)
         {
-            return View();
+            SmartBox smartBoxObj  = sbc.smartBoxes.Where(obj => obj.Id == id).FirstOrDefault();
+
+            //Se la smartbox non viene trovata
+            if (smartBoxObj == null)
+            {
+                return NotFound($"La smartbox con id {id} non è stata trovata");
+            }
+            else //Altrimenti viene passata alla vista 
+            {
+                return View("Details", smartBoxObj);
+            }
+       
         }
 
-        // GET: SmartBoxController/Create
+        // GET: SmartBoxController/Create -- Mostra solo la pagina singola "creazione"
         public ActionResult Create()
         {
-            return View();
+            SmartBox smartbox = new SmartBox();
+            return View(smartbox);
         }
 
         // POST: SmartBoxController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(SmartBox formData)
         {
-            try
+            if (!ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                //smartboxcontext aggiungi formdata dalla pagina
+                sbc.Add(formData);
+                sbc.SaveChanges();
+                return View("Create", formData);
             }
-            catch
-            {
-                return View();
-            }
+            formData = sbc.smartBoxes.FirstOrDefault();
+            sbc.smartBoxes.Add(formData);
+            sbc.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        // GET: SmartBoxController/Edit/5
+        // GET: SmartBoxController/Edit/5 -- Mostra solo la pagina singola "Modifica"
         public ActionResult Edit(int id)
         {
-            return View();
+            SmartBox smartBoxEdit = sbc.smartBoxes.Where(obj => obj.Id == id).FirstOrDefault();
+            if (smartBoxEdit == null)
+            {
+                return NotFound();
+            }
+
+            return View(smartBoxEdit);
+
         }
 
-        // POST: SmartBoxController/Edit/5
+        // POST: SmartBoxController/Edit/5 -- Invia i dati compilati
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+     
+        public ActionResult Edit(int id, SmartBox formData)
         {
-            try
+            if (!ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                //smartboxcontext aggiungi formdata dalla pagina
+                sbc.Add(formData);
+                sbc.SaveChanges();
+                return View("Edit", formData);
             }
-            catch
-            {
-                return View();
-            }
+            formData.Id = id;
+            sbc.smartBoxes.Update(formData);
+            sbc.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        // GET: SmartBoxController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
+       
         // POST: SmartBoxController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id)
         {
-            try
+            SmartBox smartBoxDelete = sbc.smartBoxes.Where(obj => obj.Id == id).FirstOrDefault();
+            if (smartBoxDelete != null)
             {
-                return RedirectToAction(nameof(Index));
+                sbc.smartBoxes.Remove(smartBoxDelete);
+                sbc.SaveChanges();
+                return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+
+            return NotFound();
+
         }
     }
 }
